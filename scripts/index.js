d0314f6154a542fdb47784890c72eaf7b7111186
Vergos1 <legendsmobajl4@gmail.com>
2024-06-11 12:29:22 +0300

    updates: add content and header sections and provide date picker inputs

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+
+// const datePickerFrom = document.getElementById('date-from');
+// datePickerFrom.localization = {
+//     buttonLabel: "Choose date",
+//     placeholder: "from",
+//     selectedDateMessage: "Selected date is",
+//     prevMonthLabel: "Previous month",
+//     nextMonthLabel: "Next month",
+//     monthSelectLabel: "Month",
+//     yearSelectLabel: "Year",
+//     closeLabel: "Close window",
+//     calendarHeading: "Choose a date",
+//     dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+//     monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+// };
+
+// const datePickerTo = document.getElementById('date-to');
+// datePickerTo.localization = {
+//     buttonLabel: "Choose date",
+//     placeholder: "to",
+//     selectedDateMessage: "Selected date is",
+//     prevMonthLabel: "Previous month",
+//     nextMonthLabel: "Next month",
+//     monthSelectLabel: "Month",
+//     yearSelectLabel: "Year",
+//     closeLabel: "Close window",
+//     calendarHeading: "Choose a date",
+//     dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+//     monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+// };
